c89c74ad9b28114370a81b887fa0baf3138c432e
Tristan Huguenot <thugueno@3D6.42angouleme.fr>
2023-05-30 13:41:14 +0200

    added parsing()

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,8 +6,15 @@
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 12:40:53 by thugueno          #+#    #+#             */
-/*   Updated: 2023/05/30 12:41:21 by thugueno         ###   ########.fr       */
+/*   Updated: 2023/05/30 13:39:52 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
+int	parsing(t_param *param, char *input)
+{
+	ft_printf("%s\n", param->prompt);
+	(void)input;
+	return (0);
+}
