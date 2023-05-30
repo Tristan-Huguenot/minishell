76079fb5d74fae955a66c8ab1a9ada42e271303e
Tristan Huguenot <thugueno@3D6.42angouleme.fr>
2023-05-30 12:33:32 +0200

    modified header, regrouped into 4 header

--- include/minishell_define.h
+++ include/minishell_define.h
@@ -1,19 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   color.h                                            :+:      :+:    :+:   */
+/*   minishell_define.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/24 14:47:02 by thugueno          #+#    #+#             */
-/*   Updated: 2023/05/24 14:52:09 by thugueno         ###   ########.fr       */
+/*   Created: 2023/05/30 11:24:02 by thugueno          #+#    #+#             */
+/*   Updated: 2023/05/30 12:30:32 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COLOR_H
-# define COLOR_H
+#ifndef MINISHELL_DEFINE_H
+# define MINISHELL_DEFINE_H
+
+/*	COLORS	*/
 
 # define NOC "\033[0m"
 # define PROMPTC "\033[0;196m"
+
+/*	ERRORS	*/
+
+# define E_ARGC 1000
+# define E_WQUOTE 1001
 
 #endif
--- include/minishell_struct.h
+++ include/minishell_struct.h
@@ -1,20 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prompt.h                                           :+:      :+:    :+:   */
+/*   minishell_struct.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/24 14:36:33 by thugueno          #+#    #+#             */
-/*   Updated: 2023/05/24 14:40:56 by thugueno         ###   ########.fr       */
+/*   Created: 2023/05/30 11:24:02 by thugueno          #+#    #+#             */
+/*   Updated: 2023/05/30 12:32:11 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PROMPT_H
-# define PROMPT_H
+#ifndef MINISHELL_STRUC_H
+# define MINISHELL_STRUC_H
 
-# include "minishell.h"
+typedef struct s_plot
+{
+	char			*cmd;
+	char			**cmd_arg;
+	int				argc;
+	char			**in;
+	char			**out;
+	struct s_plot	*next;
+}	t_plot;
 
-void	prompt_in(void);
+typedef struct s_param
+{
+	char	*progname;
+	char	*prompt;
+	char	**paths;
+	t_plot	*plots;
+}	t_param;
 
 #endif
