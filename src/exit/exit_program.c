463a70d8138a8b88cb065d727985494e0a3b4be6
Tristan Huguenot <thugueno@3D6.42angouleme.fr>
2023-05-30 12:34:59 +0200

    remove init func from main, and added exit_progam

--- src/exit/exit_program.c
+++ src/exit/exit_program.c
@@ -1,33 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prompt.c                                           :+:      :+:    :+:   */
+/*   exit_program.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/24 14:36:04 by thugueno          #+#    #+#             */
-/*   Updated: 2023/05/30 12:32:52 by thugueno         ###   ########.fr       */
+/*   Created: 2023/05/30 12:12:27 by thugueno          #+#    #+#             */
+/*   Updated: 2023/05/30 12:26:55 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	prompt_in(t_param *param)
+void	free_param(t_param *param)
 {
-	char	*input;
+	if (param && param->paths)
+		ft_free_strs(param->paths);
+	free(param);
+}
 
-	input = readline(param->prompt); // on rajoutera le chemin?
-	if (ft_strlen(input) != 0)
-	{
-		add_history(input);
-		parsing_redirection(&input, 0);
-		if (ft_strncmp(input, "exit", 5) == 0)
-		{
-			rl_clear_history();
-			free(input);
-			g_return = 0;
-			exit_program(param);
-		}
-		free(input);
-	}
+void	exit_program(t_param *param)
+{
+	free_param(param);
+	exit(g_return);
 }
--- src/parsing/wrong_quote.c
+++ src/parsing/wrong_quote.c
@@ -6,7 +6,7 @@
 /*   By: nminotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 19:29:06 by nminotte          #+#    #+#             */
-/*   Updated: 2023/05/25 00:49:36 by thugueno         ###   ########.fr       */
+/*   Updated: 2023/05/30 12:06:55 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,14 @@
 int	first_quote(char *input, int i, char q)
 {
 	char	*str;
-	int		size;
-	int		tmp;
 
+	if (!input)
+		return (-1);
 	i++;
-	tmp = i;
 	while (input[i] != q)
 	{
 		if (input[i] == '\0')
-		{
-			ft_putendl_fd("your quote is note close", 2);
-			g_return = EPERM;
 			return (-1);
-		}
 		i++;
 	}
 	return (i);
@@ -37,6 +32,8 @@ int	wrong_quote(char *input)
 {
 	int	i;
 
+	if (!input)
+		return (-1);
 	i = 0;
 	while (input[i])
 	{
@@ -45,7 +42,11 @@ int	wrong_quote(char *input)
 		if (input[i] == '\"')
 			i = first_quote(input, i, '\"');
 		if (i == -1)
+		{
+			ft_putendl_fd("your quote is note close", 2);
+			g_return = EPERM;
 			return (1);
+		}
 		i++;
 	}
 	return (0);
