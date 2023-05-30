eb2306187a24b47b1f9940db85036d8271e214a1
Nil Minotte-Cercus <nminotte@3C5.42angouleme.fr>
2023-05-30 12:46:22 +0200

    prompt

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 14:36:04 by thugueno          #+#    #+#             */
-/*   Updated: 2023/05/24 14:55:30 by thugueno         ###   ########.fr       */
+/*   Updated: 2023/05/24 20:12:41 by nminotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	prompt_in(void)
 	if (ft_strlen(input) != 0)
 	{
 		add_history(input);
-		parsing_redirection(&input, 0);
+		wrong_quote(input);
+		// parsing_redirection(&input, 0);
 		if (ft_strncmp(input, "exit", 5) == 0)
 		{
 			rl_clear_history();
