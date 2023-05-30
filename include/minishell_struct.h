bf7958d49a82a59efb9bd5896f8375bcec8accdf
Nil Minotte-Cercus <nminotte@1E2.42angouleme.fr>
2023-05-30 16:40:22 +0200

    creation de la struct env

--- include/minishell_struct.h
+++ include/minishell_struct.h
@@ -6,12 +6,12 @@
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 11:24:02 by thugueno          #+#    #+#             */
-/*   Updated: 2023/05/30 13:20:20 by thugueno         ###   ########.fr       */
+/*   Updated: 2023/05/30 16:39:30 by nminotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_STRUC_H
-# define MINISHELL_STRUC_H
+#ifndef MINISHELL_STRUCT_H
+# define MINISHELL_STRUCT_H
 
 typedef struct s_plot
 {
@@ -23,12 +23,20 @@ typedef struct s_plot
 	struct s_plot	*next;
 }	t_plot;
 
+typedef struct s_env
+{
+	char			*var;
+	char			*content;
+	struct s_env	*next;
+}	t_env;
+
 typedef struct s_param
 {
 	char	*progname;
 	char	*prompt;
 	char	**paths;
 	t_plot	*plots;
+	t_env	*env;
 }	t_param;
 
 #endif
