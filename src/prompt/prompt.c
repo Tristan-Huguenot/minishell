efd66e9ef97ae4c80bce0870e64f32ae0137829b
joqu3r <huguenot.tristan@gmail.com>
2023-05-31 03:04:11 +0200

    added wrong quote verification and for now if its good just echo the command

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -1,25 +1,40 @@
 #include "minishell.h"
 
+void	exit_tmp(t_param *param, char **input_ptr)
+{
+	/* exit avec bash peut avoir un argument (0-133 inclut) et fonctionne avec les pipes du genre exit 1 | sleep 5
+		j ai mis en temporaire psk faudra le gerer une fois le parsing fait
+		au moment d executer la cmd > si param->plot->cmd_arg[0] == keyword alors on appel built in
+	*/
+	free(*input_ptr);
+	rl_clear_history();
+	g_return = 0;
+	exit_program(param);
+}
+
 void	prompt_in(t_param *param)
 {
 	char	*input;
+	char	quote;
 
 	input = readline(param->prompt);
 	if (ft_strlen(input) != 0)
 	{
 		add_history(input);
 		if (ft_strncmp(input, "exit", 5) == 0)
-		{
-			rl_clear_history();
-			free(input);
-			g_return = 0;
-			exit_program(param);
-		}
-		if (!parsing(param, input))
+			exit_tmp(param, &input);
+		quote = wrong_quote(input);
+		if (quote)
+			error_handler(E_WQUOTE, param, &quote);
+		else
+			ft_printf("%s\n", input);
+		/*if (!parsing(param, input))
 		{		
-			//execut();
-			//free_chained_link();
-		}
+			execut();
+			free_chained_link();
+		}*/
 		free(input);
 	}
+	else
+		free(input);
 }
