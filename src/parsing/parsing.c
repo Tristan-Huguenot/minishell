01a159c3613bfba508edee27e26cff5944e40b8a
joqu3r <huguenot.tristan@gmail.com>
2023-06-01 02:27:53 +0200

    replace the tmp plot to the new one

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -5,13 +5,14 @@ int	parsing_plot(t_param *param)
 	t_plot	*tmp;
 
 	tmp = param->plots;
-	parsing_variable(param->plots, param->env);
+	//parsing_variable(param->plots, param->env);
 	while (tmp)
 	{
 		// parsing_variable(tmp->cmd, param->env);
 		// if (parsing_redir(tmp, param))
 			// return (1);
 		// parsing_token(tmp);
+		ft_printf("%s\n", tmp->cmd);
 		tmp= tmp->next;
 	}
 	return (0);
@@ -26,13 +27,9 @@ int	parsing(t_param *param, char *input)
 		error_handler(E_WQUOTE, param, &quote);
 	else
 	{
-		ft_printf("%s\n", input);// int ret;
-		param->plots = ft_calloc(1, sizeof(*param->plots));
-		param->plots->cmd = ft_strdup(input);
-		// init_plots(param, input);
+		//ft_printf("%s\n", input);// int ret;
+		init_plots(param, input);
 		parsing_plot(param);
-		free(param->plots->cmd);
-		free(param->plots);
 	}
 	return (0);
 }
