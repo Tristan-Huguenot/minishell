57de37353237a0e7eb6610dbcfaab9fc00697c97
joqu3r <huguenot.tristan@gmail.com>
2023-06-04 00:27:39 +0200

    made function to take redirm put them in plot->redir and delete from plot->cmd, now need to separate arg

---
+++ src/parsing/parsing_redir_move.c
@@ -0,0 +1,88 @@
+#include "minishell.h"
+
+static char	*delete_redir(char *cmd, int redir)
+{
+	char	*first;
+	char	*last;
+
+	first = ft_substr(cmd, 0, redir);
+	if (cmd[redir] && cmd[redir] == cmd[redir + 1])
+		redir++;
+	redir++;
+	while (ft_char_in_set(cmd[redir], CS_WSPACE))
+		redir++;
+	while (cmd[redir] && !ft_char_in_set(cmd[redir], CS_WSPACE))
+	{
+		if (ft_char_in_set(cmd[redir], CS_QUOTE))
+			redir = first_quote(cmd, redir, cmd[redir]);
+		redir++;
+	}
+	last = ft_strdup(cmd + redir);
+	first = ft_strjoin_free(first, last);
+	free(last);
+	return (first);
+}
+
+static char	*get_redir_name(char *cmd, int redir)
+{
+	char	*name;
+	int		end;
+
+	if (cmd[redir] && cmd[redir] == cmd[redir + 1])
+		redir++;
+	redir++;
+	while (ft_char_in_set(cmd[redir], CS_WSPACE))
+		redir++;
+	end = redir;
+	while (cmd[end] && !ft_char_in_set(cmd[end], CS_WSPACE))
+	{
+		if (ft_char_in_set(cmd[end], CS_QUOTE))
+			end = first_quote(cmd, end, cmd[end]);
+		end++;
+	}
+	name = ft_substr(cmd, redir, end - redir);
+	return (name);
+}
+
+static char	*get_redir_type(char *cmd, int redir)
+{
+	char	*type;
+
+	type = ft_calloc(4, sizeof(*type));
+	if (!type)
+		return (type);
+	if (cmd[redir] == '<')
+		type[0] = '0';
+	else
+		type[0] = '1';
+	if (cmd[redir] && cmd[redir] == cmd[redir + 1])
+		type[1] = '2';
+	else
+		type[1] = '1';
+	type[2] = '=';
+	return (type);
+}
+
+void	move_redir(t_plot *plot, int size)
+{
+	char	*tmp;
+	char	*name;
+	int		redir;
+	int		i;
+
+	i = 0;
+	while (i < size)
+	{
+		redir = jump_next_redir(plot->cmd, 0);
+		tmp = get_redir_type(plot->cmd, redir);
+		name = get_redir_name(plot->cmd, redir);
+		tmp = ft_strjoin_free(tmp, name);
+		free(name);
+		plot->redir[i] = tmp;
+		tmp = delete_redir(plot->cmd, redir);
+		free(plot->cmd);
+		plot->cmd = tmp;
+		i++;
+	}
+
+}
