22f6f666d99412576240c82bd7002aabdf5c71ff
joqu3r <huguenot.tristan@gmail.com>
2023-06-03 17:35:42 +0200

    added func to proprely see plots when parsing finish

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -12,6 +12,43 @@ void	exit_tmp(t_param *param, char **input_ptr)
 	exit_program(param);
 }
 
+void	print_strs(char **strs)
+{
+	int	i;
+
+	i = 0;
+	while (strs[i])
+	{
+		ft_printf("\t\t%s\n", strs[i]);
+		i++;
+	}
+}
+
+void	print_plots(t_plot *plots)
+{
+	int	i;
+
+	i = 1;
+	while (plots)
+	{
+		ft_printf("plot %d:\n", i);
+		if (plots->cmd)
+			ft_printf("\tcmd : %s\n", plots->cmd);
+		if (plots->cmd_arg)
+		{
+			ft_printf("\tcmd_arg :\n", i);
+			print_strs(plots->cmd_arg);
+		}
+		if (plots->redir)
+		{
+			ft_printf("\tredir :\n", i);
+			print_strs(plots->redir);
+		}
+		i++;
+		plots = plots->next;
+	}
+}
+
 void	prompt_in(t_param *param)
 {
 	char	*input;
@@ -24,7 +61,7 @@ void	prompt_in(t_param *param)
 		if (ft_strncmp(input, "exit", 5) == 0)
 			exit_tmp(param, &input);
 		if (!parsing(param, input))
-			ft_printf("No error\n");
+			print_plots(param->plots);
 		/*if (!parsing(param, input))
 		{		
 			execut();
