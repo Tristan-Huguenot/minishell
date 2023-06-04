a07318c516ea9339942779da6ca53608d8634279
joqu3r <huguenot.tristan@gmail.com>
2023-06-04 02:25:20 +0200

    move arg from plot->cmd to plot->cmd_arg

---
+++ src/parsing/parsing_arg.c
@@ -0,0 +1,46 @@
+#include "minishell.h"
+
+static int	count_arg(char *str)
+{
+	int	c;
+	int	i;
+
+	c = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (ft_char_in_set(str[i], CS_WSPACE))
+			i++;
+		if (!str[i])
+			return (c);
+		c++;
+		i = jump_next_charset(str, i, CS_WSPACE);
+	}
+	return (c);
+}
+
+void	parsing_arg(t_plot *plot)
+{
+	char	*tmp;
+	int		i;
+	int		start;
+	int		end;
+
+	plot->argc = count_arg(plot->cmd);
+	plot->cmd_arg = ft_calloc(plot->argc + 1, sizeof(*plot->cmd_arg));
+	if (!plot->cmd_arg)
+		return ;
+	i = 0;
+	while (i < plot->argc)
+	{
+		start = 0;
+		while (ft_char_in_set(plot->cmd[start], CS_WSPACE))
+			start++;
+		end = jump_next_charset(plot->cmd, start, CS_WSPACE);
+		plot->cmd_arg[i] = ft_substr(plot->cmd, start, end - start);
+		tmp = ft_strdup(plot->cmd + end);
+		free(plot->cmd);
+		plot->cmd = tmp;
+		i++;
+	}
+}
