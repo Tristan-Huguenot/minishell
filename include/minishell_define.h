7f0bbe1720a5ccea40e6777630a649a8442cbfe8
joqu3r <huguenot.tristan@gmail.com>
2023-06-09 00:38:51 +0200

    modified error define to an enum

--- include/minishell_define.h
+++ include/minishell_define.h
@@ -6,7 +6,7 @@
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 11:24:02 by thugueno          #+#    #+#             */
-/*   Updated: 2023/06/07 15:00:43 by nminotte         ###   ########.fr       */
+/*   Updated: 2023/06/09 00:07:59 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,23 @@
 # define NOC     "\001\e[0m\002"            /* "\033\001[0m"*/
 # define PROMPTC "\001\e[1m\e[38;5;207m\002"         /*"\033\001[38;5;207m"*/
 
-/*	ERRORS	*/
-
-# define E_ARGC 1000
-# define E_WQUOTE 1001
-# define E_TOKEN 1002
-
 /*	CHARSET	*/
 
 # define CS_WSPACE " \b\t\n\v\f\r"
 # define CS_QUOTE "\'\""
 # define CS_REDIR "<>"
 
+/*	ENUM ERRORS	*/
+
+enum e_error {
+	E_ARGC = 1000,
+	E_WQUOTE,
+	E_TOKEN,
+};
+
 /* ENUM BUILTIN */
 
-enum {
+enum e_builtin {
 	ECHO = 1,
 	CD,
 	PWD,
