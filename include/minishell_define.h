40b7e840b0197631dfeede0a896fdc56a37d3829
joqu3r <huguenot.tristan@gmail.com>
2023-06-10 17:02:51 +0200

    added define for unmanage metacharacter (token)

--- include/minishell_define.h
+++ include/minishell_define.h
@@ -6,7 +6,7 @@
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 11:24:02 by thugueno          #+#    #+#             */
-/*   Updated: 2023/06/09 02:00:07 by thugueno         ###   ########.fr       */
+/*   Updated: 2023/06/10 17:02:08 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 
 /*	COLORS	*/
 
-# define NOC     "\001\e[0m\002"            /* "\033\001[0m"*/
-# define PROMPTC "\001\e[1m\e[38;5;207m\002"         /*"\033\001[38;5;207m"*/
+# define NOC     "\001\e[0m\002"
+# define PROMPTC "\001\e[1m\e[38;5;207m\002"
 
 /*	CHARSET	*/
 
 # define CS_WSPACE " \b\t\n\v\f\r"
 # define CS_QUOTE "\'\""
 # define CS_REDIR "<>"
-# define CS_IDENTIFIER ""
+# define CS_UTOKEN "\\\;\#\&\(\)"
 
 /*	ENUM ERRORS	*/
 
@@ -31,6 +31,7 @@ enum e_error {
 	E_ARGC = 1000,
 	E_WQUOTE,
 	E_TOKEN,
+	E_UTOKEN,
 	E_IDENTIFIER,
 };
 
