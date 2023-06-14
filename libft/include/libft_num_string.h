ebf8c874bb8d059889d275839508e6bc79916ac1
joqu3r <huguenot.tristan@gmail.com>
2023-06-14 15:35:47 +0200

    added proto and new func ft_ltoa long to char

--- libft/include/libft_num_string.h
+++ libft/include/libft_num_string.h
@@ -6,7 +6,7 @@
 /*   By: thugueno <thugueno@student.42angoulem      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 18:44:44 by thugueno          #+#    #+#             */
-/*   Updated: 2023/02/04 18:55:11 by thugueno         ###   ########.fr       */
+/*   Updated: 2023/06/14 15:35:05 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # include "libft_string.h"
 
 int		ft_atoi(const char *nptr);
+long	ft_atol(const char *nptr);
 char	*ft_itoa(int n);
+char	*ft_ltoa(long n);
 char	*ft_ultoa_base(unsigned long long int nb, char *base);
 
 #endif
