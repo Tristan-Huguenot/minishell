9865c6ee55fb88519700823076d512dbd2ae736a
joqu3r <huguenot.tristan@gmail.com>
2023-07-10 22:15:02 +0200

    replace include libft.h by .h of modules used

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nminotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 17:46:39 by nminotte          #+#    #+#             */
-/*   Updated: 2023/06/20 17:30:51 by nminotte         ###   ########.fr       */
+/*   Updated: 2023/07/10 22:06:57 by thugueno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@
 # include <dirent.h>
 # include <sys/types.h>
 # include <signal.h>
-# include "libft.h"
 # include <sys/wait.h>
+# include "libft_ft_printf.h"
+# include "libft_file.h"
 # include "minishell_define.h"
 # include "minishell_struct.h"
 # include "minishell_proto.h"
